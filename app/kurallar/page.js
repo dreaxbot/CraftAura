ba142058518d5ef988836949d246a65dc82a975d
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 20:24:42 +0300

    Update page.js

--- app/kurallar/page.js
+++ app/kurallar/page.js
@@ -1,6 +1,6 @@
 export default function Kurallar() {
   return (
-    <div className="mt-20 text-center">
+    <div className="mt-32 text-center">
       <h2 className="text-3xl font-bold mb-4 text-green-400">📜 Sunucu Kuralları</h2>
       <ul className="list-disc list-inside space-y-2 text-lg">
         <li>Küfür ve hakaret yasaktır.</li>
