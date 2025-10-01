f6df342625af79f2584636bbd316fcee870c676f
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 20:17:20 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
 export default function Navbar() {
   return (
     <nav className="bg-gray-800/80 backdrop-blur-md p-4 flex justify-between items-center w-full shadow-lg fixed top-0 z-20">
-      <img src="/logo.png" alt="Sunucu Logo" className="h-10 w-10"/>
-      <h1 className="text-2xl font-bold text-green-400 ml-2">Minecraft Sunucum</h1>
+      <div className="flex items-center">
+        <img src="/logo.png" alt="Sunucu Logo" className="h-12 w-12 mr-2" />
+        <h1 className="text-2xl font-bold text-green-400">Minecraft Sunucum</h1>
+      </div>
       <div className="space-x-6">
         <a href="/" className="hover:text-green-300 transition">Ana Sayfa</a>
         <a href="/kurallar" className="hover:text-green-300 transition">Kurallar</a>
