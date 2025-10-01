6b734af600337d2adad8e85c4948f4f74164bb69
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 18:47:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mcgreen: "#3C8527", // Minecraft yeşili
+      },
+    },
+  },
+  plugins: [],
+};
 
