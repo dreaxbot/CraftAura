5781b7c890c8f356552d48d521c79b2b0caec18d
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 20:13:14 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <body className="bg-gray-900 text-white font-sans relative">
         <div 
           className="fixed inset-0 -z-10 w-full h-full bg-cover bg-center"
-          style={{ backgroundImage: "url('/galaxy.gif')" }}
+          style={{ backgroundImage: "url('/images/galaxy.gif')" }}
         />
         <Navbar />
         <main className="relative z-10 min-h-screen p-6 flex flex-col items-center">
