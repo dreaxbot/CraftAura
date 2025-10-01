445eef3720b2d3104c9175b7589ed8da42f98057
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 20:24:59 +0300

    Update page.js

--- app/ekip/page.js
+++ app/ekip/page.js
@@ -6,7 +6,7 @@ export default function Ekip() {
   ];
 
   return (
-    <div className="mt-20 text-center">
+    <div className="mt-32 text-center">
       <h2 className="text-3xl font-bold mb-4 text-green-400">👥 Ekip</h2>
       <ul className="space-y-3">
         {ekip.map((uye, i) => (
