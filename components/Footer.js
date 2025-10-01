441ee6517dd9e0fa5afc1701826aa4a5a08a7b42
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 19:12:01 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-center p-4 mt-6 border-t border-gray-700">
+      © 2025 Minecraft Sunucum | Tüm Hakları Saklıdır
+    </footer>
+  );
+}
 
