bb06feba490a450c4b16f08204da0599f282b9e9
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 20:05:16 +0300

    Update page.js

--- app/ekip/page.js
+++ app/ekip/page.js
@@ -6,13 +6,13 @@ export default function Ekip() {
   ];
 
   return (
-    <div>
+    <div className="mt-20 text-center">
       <h2 className="text-3xl font-bold mb-4 text-green-400">👥 Ekip</h2>
       <ul className="space-y-3">
         {ekip.map((uye, i) => (
           <li
             key={i}
-            className="bg-gray-800 p-4 rounded-2xl shadow-lg hover:shadow-green-500 transition"
+            className="bg-gray-800/70 backdrop-blur-md p-4 rounded-2xl shadow-lg hover:shadow-green-500 transition"
           >
             <b>{uye.isim}</b> - {uye.rol}
           </li>
