6db58efaa628d47ad61831ebb95bbe3fcbb953d9
dreaxbot <bayramfatihmehmed@gmail.com>
2025-10-01 18:50:11 +0300

    Update page.js

--- app/kurallar/page.js
+++ app/kurallar/page.js
@@ -1,1 +1,14 @@
+export default function Kurallar() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold mb-4">📜 Sunucu Kuralları</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>Küfür ve hakaret yasaktır.</li>
+        <li>Hile / X-Ray / Hack yasaktır.</li>
+        <li>Spam veya reklam yapmak yasaktır.</li>
+        <li>Diğer oyunculara saygılı olun.</li>
+      </ul>
+    </div>
+  );
+}
 
